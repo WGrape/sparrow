0b6f5e5f0cc01fc63686c3dc2aac39493ee98bb5
lvsi <15239928381@163.com>
2024-04-11 16:08:19 +0800

    feat: add azkaban support

--- azkaban/hooks/after_start.sh
+++ azkaban/hooks/after_start.sh
@@ -2,3 +2,5 @@
 
 # include sdk of sparrow.
 . .work/include/sdk.sh
+
+docker exec -d sparrow_container_${CONTAINER_NAMESPACE}_azkaban bash /home/azkaban/azkaban-solo-server/build/install/azkaban-solo-server/bin/start-solo.sh
